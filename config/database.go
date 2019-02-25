9bb08fd27224b62154267341771a1cfabe93c28d
Sinar Dyas <sinardyas@Sinars-MacBook-Pro.local>
2019-02-25 12:35:29 +0700

    change package name for database connetor

--- config/database.go
+++ config/database.go
@@ -1,4 +1,4 @@
-package config
+package database
 
 import (
 	"github.com/jinzhu/gorm"
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm/dialects/mysql" // dialect
-	"github.com/sinardyas/golang-crud/config"
+	database "github.com/sinardyas/golang-crud/config"
 	"github.com/sinardyas/golang-crud/controllers"
 )
 
@@ -11,7 +11,7 @@ func main() {
 	router := gin.Default()
 	userRoute := router.Group("user")
 
-	db := config.Init()
+	db := database.Init()
 
 	controllers.Init(db, userRoute)
 	router.Run()

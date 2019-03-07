8338fc314a6c793c591583f967fc9c9be5d5a078
Sinar Dyas <sinardyas@Sinars-MacBook-Pro.local>
2019-03-07 15:02:33 +0700

    add path prefix for user api

--- routers/user.go
+++ routers/user.go
@@ -14,9 +14,10 @@ func (*UserRouter) UserRouterHandling(router *mux.Router) {
 	var auth helpers.Auth
 	var userController controllers.UserController
 
-	router.HandleFunc("/", auth.MiddlewareAuth(userController.Get)).Methods("GET")
-	router.HandleFunc("/", auth.MiddlewareAuth(userController.Create)).Methods("POST")
-	router.HandleFunc("/{id:[0-9]+}", auth.MiddlewareAuth(userController.Update)).Methods("PUT")
-	router.HandleFunc("/{id:[0-9]+}", auth.MiddlewareAuth(userController.Delete)).Methods("DELETE")
-	router.HandleFunc("/login", userController.Login).Methods("POST")
+	subrouter := router.PathPrefix("/user/").Subrouter()
+	subrouter.HandleFunc("/", auth.MiddlewareAuth(userController.Get)).Methods("GET")
+	subrouter.HandleFunc("/", auth.MiddlewareAuth(userController.Create)).Methods("POST")
+	subrouter.HandleFunc("/{id:[0-9]+}", auth.MiddlewareAuth(userController.Update)).Methods("PUT")
+	subrouter.HandleFunc("/{id:[0-9]+}", auth.MiddlewareAuth(userController.Delete)).Methods("DELETE")
+	subrouter.HandleFunc("/login", userController.Login).Methods("POST")
 }
